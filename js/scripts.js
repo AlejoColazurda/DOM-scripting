8d67f97625de54e65ad498ae2f01008315533a6a
Alejo <acolazurda@gmail.com>
2024-03-27 21:26:29 -0300

    Generando un enlace con JS

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,28 @@ const enlaces = document.querySelectorAll('.navegacion a');//querySelectorAll si
 console.log(enlaces);
 // enlaces[0].textContent = 'Nuevo Texto Para Enlace';
 // enlaces[0].href = 'http://google.com/';
-// enlaces[0].classList.add('nueva-clase');
+ enlaces[0].classList.add('nueva-clase');
 // enlaces[0].classList.remove('nueva-clase');
 
 //getElementById
-const heading2 = documentx.getElementById('heading');
-console.log(heading2);
+// const heading2 = documentx.getElementById('heading');
+// console.log(heading2);
+
+//Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A');
+
+//Argegar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+//Agregar el texto
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
+
+//Agregar la clase
+nuevoEnlace =classList.add('navegacion__enlace');
+
+//Agregar todo lo anterior al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace)
+
+console.log(nuevoEnlace);
+
