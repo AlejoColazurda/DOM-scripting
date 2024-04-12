f7d0feb5646eb34d47c2b24b4f413d735f1abbfb
Alejo <acolazurda@gmail.com>
2024-04-11 22:59:00 -0300

    eventos de los inputs y textarea

--- js/EventosEnJs.js
+++ js/EventosEnJs.js
@@ -1,32 +1,52 @@
-//Eventos
-console.log(1);
+// //Eventos
+// console.log(1);
 
-window.addEventListener('load', function(){//load espera que el JS y los archivos que dependen del HTML estén listos
-    console.log(2);
-})
+// window.addEventListener('load', function(){//load espera que el JS y los archivos que dependen del HTML estén listos
+//     console.log(2);
+// })
 
-//otra manera de ahcer lo mismo sería así:
-window.onload = function(){
-    console.log(3);
-}
+// //otra manera de ahcer lo mismo sería así:
+// window.onload = function(){
+//     console.log(3);
+// }
 
-document.addEventListener('DOMContentLoaded', function(){//solo espera por el HTML, pero no espera CSS o imagenes.
-    console.log(4);
-});
+// document.addEventListener('DOMContentLoaded', function(){//solo espera por el HTML, pero no espera CSS o imagenes.
+//     console.log(4);
+// });
 
-Console.log(5);
+// Console.log(5);
 
-window.onscroll = function(){
-    console.log('scrolling...');
-};
+// window.onscroll = function(){
+//     console.log('scrolling...');
+// };
 
-const btnEnviar = document.querySelector('.boton--orimario');
+const btnEnviar = document.querySelector('.boton--primario');
 btnEnviar.addEventListener('click', function(evento){
     console.log(evento);
     evento.preventDefault();//previene el evento natural a lo que se asocie, por ejemplo en este caso hace que el formulario no se envie. Podríamos usarlo para que se verifique que todos los campos del formulario estan completos.
-    
-
-
-
     console.log('Enviando...');
 });
+
+//Eventos de los Inputs y textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+
+function leerTexto(e){
+    datos[e.target.id] = e.target.value;
+
+    console.log(datos);
+}
